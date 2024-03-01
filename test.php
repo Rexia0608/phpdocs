bdc9ca7c29b199ac7ed5a17647adacd2290bc54c
Rexia0608 <johnreycejas0608@gmail.com>
2024-03-01 17:07:38 +0800

    php docs array samples

---
+++ test.php
@@ -0,0 +1,43 @@
+<?php
+
+// simple array simple array  simple array  simple array //
+// $stuck = array();
+// $tracks[] = "Web Fundimental";
+// $tracks[] = "PHP";
+// $tracks[] = "JS";
+
+    /*one dimensional array /*/
+// $tracks = array("Web Fundimental", "PHP","JS"); 
+
+//  echo "Track 1 . $tracks[0]" . "<br>";
+//  echo "Track 1 . $tracks[2]" . "<br>";
+//  echo "Track 1 . $tracks[1]" . "<br>";
+
+    /*two dimensional array /*/
+   // $tracks = array(array("Web Fundimental", 1), array("PHP", 2), array("JS", 3),);
+
+        // echo "Tracks 1: " . $tracks[0][0]. " " . $tracks[2][1];     /*/ output: Tracks 1: Web Fundimental 3Tracks 1: JS /*/
+       // echo "Tracks 1: " . $tracks[2][0]. "<br>";              /*/ output: Tracks 1: JS /*/
+      //  echo "Tracks 1: " . $tracks[1][1]. "<br>";               /*/ output: Tracks 1: 2 /*/
+
+        // Associative Array (key value pair)//
+        // $tracks = array("course_name" => "Web Fundimental",
+        //                  "weeks" => 4);
+
+        // echo $tracks["course_name"]. "<br>";    /*/ output: Web fundimental /*/
+        // echo $tracks["weeks"]. "<br>";          /*/ output: 4 /*/
+
+
+        // two dimensional Associative Array 
+    //        $tracks = array(
+    //        $tracks = array("course_name" => "Web Fundimental", "weeks" => 4),
+    //        $tracks = array("course_name" => "PHP", "weeks" => 4),
+    //        $tracks = array("course_name" => "JS", "weeks" => 2)
+    //    );
+    //    echo $tracks[0]["course_name"] ." ". $tracks[0]["course_name"]. "<br>";    /*/ output: Webfundimental /*/
+    //    echo $tracks[1]["course_name"];      /*/ output: PHP  /*/   
+    //    echo $tracks[2]["course_name"];      /*/ output: JS /*/
+
+
+
+?>
